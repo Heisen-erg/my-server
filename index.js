31537b42981fb8264d7faf3b9daff897a5761a79
Heisen-erg <heisenberg.database.1@gmail.com>
2023-08-18 09:30:30 +0530

    first server commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const Data = require("./schema")
+const cors = require("cors")
+const mongoose = require("mongoose")
+
+const app = express();
+
+app.use(cors({credentials:true,origin:"http://localhost:3000"}));
+app.use(express.json());
+
+app.get("/",(req,res) => {
+  res.json({yoyo:"yiyiyi"})
+})
+app.post("/r", async (req,res) => {
+  await mongoose.connect("mongodb+srv://heisenbergdatabase1:uDVtMXlxymOMO8eX@cluster0.vwlkprw.mongodb.net/")
+    try{const{ name,email} = req.body
+   if(!name){
+     res.json({error:"name cannot be empty"})
+   }
+    const exist = await Data.findOne({email})
+   if(exist){
+    res.json({error:"email is already registered"})
+   }
+   const user = Data.create(req.body)
+   res.json({message:"successfully registered"})
+  }catch(error) { console.log(error)}
+})
+
+
+
+app.listen(4000,() => console.log("running"));
---
+++ schema.js
@@ -0,0 +1,17 @@
+const { Schema } = require("mongoose");
+const mongoose = require("mongoose")
+const user = new Schema(
+    {
+        name: {
+            
+            type:String
+        },
+        email:{
+            
+            type:String
+        }
+    });
+
+const Data = new mongoose.model("userdata",user)
+
+module.exports = Data;
